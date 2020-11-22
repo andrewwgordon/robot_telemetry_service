791d0d7bde0406ecc1e99ed6fcc4729f0fe70c73
Andrew Gordon <andrew.gordon2@baesystems.com>
2020-11-22 15:07:27 +0000

    Added logging

--- robottelemetryservice/infrastructure/udp/udp_measurement_event_handler.py
+++ robottelemetryservice/infrastructure/udp/udp_measurement_event_handler.py
@@ -1,12 +1,15 @@
+import logging
 import socketserver
 import threading
 from robottelemetryservice.infrastructure.serialisation import serializer
 from robottelemetryservice.infrastructure.configuration import application
 
+log=logging.getLogger(__name__)
 measurement_repository=application.get_measurement_repository()
 
 class UDPMeasurementEventHandler(socketserver.DatagramRequestHandler):
     def handle(self):
         udp_payload=self.request[0].strip().decode()
-        measurements=serializer.udp_msg_to_measurements(udp_payload)
-        measurement_repository.add(measurements)
+        log.info(udp_payload)
+        # measurements=serializer.udp_msg_to_measurements(udp_payload)
+        # measurement_repository.add(measurements)
